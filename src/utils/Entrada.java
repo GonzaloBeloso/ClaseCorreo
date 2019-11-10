b0d1794b81d4b26aa489d85a3bc0a505749cc8ad
Gonzalo Beloso <gonzalobelosocuesta@gmail.com>
2019-11-10 18:57:06 +0100

    Initial commit

---
+++ src/utils/Entrada.java
@@ -0,0 +1,18 @@
+package utils;
+
+import utils.Ventana;
+
+import javax.swing.*;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Ventana v = new Ventana();
+                v.initGUI();
+            }
+        });
+    }
+}
---
+++ src/utils/Ventana.java
@@ -0,0 +1,38 @@
+package utils;
+
+import paneles.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana extends JFrame {
+
+    Container container;
+    // panel personalizado
+    PestaniaUno pestaniaUno;
+    JTabbedPane panelPestanias;
+
+    public void initGUI(){
+
+        instancias();
+        configurarContainer();
+        this.setSize(new Dimension(300,300));
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        //this.pack();
+    }
+
+    private void configurarContainer() {
+        container.add(panelPestanias);
+        panelPestanias.addTab("Correo",pestaniaUno);
+
+    }
+
+    private void instancias() {
+        container = this.getContentPane();
+        panelPestanias = new JTabbedPane(SwingConstants.TOP);
+        pestaniaUno = new PestaniaUno();
+
+    }
+}
